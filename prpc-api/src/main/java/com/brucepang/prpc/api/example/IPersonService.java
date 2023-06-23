b450e9f9c43f1a4ebb214d0103828cb1eb49a29c
BrucePang <codingpang@outlook.com>
2023-06-23 15:28:18 +0800

    创建demo实例，将prpc-api作为公共代码工程

---
+++ prpc-api/src/main/java/com/brucepang/prpc/api/example/IPersonService.java
@@ -0,0 +1,13 @@
+package com.brucepang.prpc.api.example;
+
+/**
+ * @author BrucePang
+ */
+public interface IPersonService {
+    /**
+     *
+     * @param name 保存用户信息
+     * @return
+     */
+    String save(String name);
+}
