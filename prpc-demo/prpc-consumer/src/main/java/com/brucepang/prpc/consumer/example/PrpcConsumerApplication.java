18c1d17b5dfecf139c23dbb15950362ace7f6e9c
BrucePang <125526597+bruce-pang@users.noreply.github.com>
2023-09-19 06:32:22 +0800

    3-删除历史注释代码

--- prpc-demo/prpc-consumer/src/main/java/com/brucepang/prpc/consumer/example/PrpcConsumerApplication.java
+++ prpc-demo/prpc-consumer/src/main/java/com/brucepang/prpc/consumer/example/PrpcConsumerApplication.java
@@ -2,8 +2,6 @@ package com.brucepang.prpc.consumer.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 
 /**
  * @author BrucePang
