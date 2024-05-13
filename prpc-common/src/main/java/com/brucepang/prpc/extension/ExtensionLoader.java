3a67621cb6c779e94711d9c553f6cf06aedb109c
brucepang <brucepang0618@gmail.com>
2024-05-13 23:35:14 +0800

    feature(ExtensionLoader): Add StrUtil, loadResource() and loadClass()

--- prpc-common/src/main/java/com/brucepang/prpc/extension/ExtensionLoader.java
+++ prpc-common/src/main/java/com/brucepang/prpc/extension/ExtensionLoader.java
@@ -1,9 +1,13 @@
 package com.brucepang.prpc.extension;
 
+import com.brucepang.prpc.util.StrUtil;
 import com.sun.org.slf4j.internal.Logger;
 import com.sun.org.slf4j.internal.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Enumeration;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -103,7 +107,7 @@ public class ExtensionLoader<T> {
             if (urls != null) {
                 while (urls.hasMoreElements()) {
                     URL url = urls.nextElement();
-                    loadResource(extensionClasses, classLoader, url);
+                    loadResource(extensionClasses, classLoader, url, false);
                 }
             }
         } catch (Throwable t) {
@@ -111,7 +115,53 @@ public class ExtensionLoader<T> {
         }
     }
 
-    private void loadResource(Map<String, Class<?>> extensionClasses, ClassLoader classLoader, URL url) {
-        // todo: load the extension classes
+    private void loadResource(Map<String, Class<?>> extensionClasses, ClassLoader classLoader, URL resourceURL, boolean overridden) {
+        try {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(resourceURL.openStream(), StandardCharsets.UTF_8))) {
+                String line;
+                String clazz = null;
+                while ((line = reader.readLine()) != null) {
+                    final int ci = line.indexOf('#');
+                    if (ci >= 0) {
+                        line = line.substring(0, ci);
+                    }
+                    line = line.trim();
+                    if (line.length() > 0) {
+                        try {
+                            String name = null;
+                            int i = line.indexOf('=');
+                            if (i > 0) {
+                                name = line.substring(0, i).trim();
+                                clazz = line.substring(i + 1).trim();
+                            } else {
+                                clazz = line;
+                            }
+                            if (StrUtil.isNotEmpty(clazz)) {
+                                loadClass(extensionClasses, resourceURL, Class.forName(clazz, true, classLoader), name, overridden);
+                            }
+                        } catch (Throwable t) {
+                            IllegalStateException e = new IllegalStateException(
+                                    "Failed to load extension class (interface: " + type + ", class line: " + line + ") in " + resourceURL +
+                                            ", cause: " + t.getMessage(), t);
+                        }
+                    }
+                }
+            }
+        } catch (Throwable t) {
+            log.error("Exception occurred when loading extension class (interface: " +
+                    type + ", class file: " + resourceURL + ") in " + resourceURL, t);
+        }
+
     }
+
+
+    private void loadClass(Map<String, Class<?>> extensionClasses, URL url, Class<?> clazz, String name, boolean overridden) {
+        Class<?> c = extensionClasses.get(name);
+        if (c == null || overridden){
+            extensionClasses.put(name, clazz);
+        } else if (c != clazz){
+            throw new IllegalStateException("Duplicate extension " + type.getName() + " name " + name + " on " + c.getName() + " and " + clazz.getName());
+        }
+    }
+
 }
