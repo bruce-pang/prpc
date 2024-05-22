2a6ed3b3c1ec7a1c671458280994bee6f26040aa
brucepang <brucepang0618@gmail.com>
2024-05-22 08:21:46 +0800

    feature(common#util): Add Assert.java

---
+++ prpc-common/src/main/java/com/brucepang/prpc/util/Assert.java
@@ -0,0 +1,34 @@
+package com.brucepang.prpc.util;
+
+public class Assert {
+
+    public static void assertEquals(Object expected, Object actual) {
+        if ((expected == null && actual != null) || !expected.equals(actual)) {
+            throw new AssertionError("Expected " + expected + " but got " + actual);
+        }
+    }
+
+    public static void assertTrue(boolean condition) {
+        if (!condition) {
+            throw new AssertionError("Expected true but got false");
+        }
+    }
+
+    public static void assertFalse(boolean condition) {
+        if (condition) {
+            throw new AssertionError("Expected false but got true");
+        }
+    }
+
+    public static void assertNotNull(Object object) {
+        if (object == null) {
+            throw new AssertionError("Expected object to be not null");
+        }
+    }
+
+    public static void assertNull(Object object) {
+        if (object != null) {
+            throw new AssertionError("Expected object to be null");
+        }
+    }
+}
