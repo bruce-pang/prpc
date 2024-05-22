30336e34d8d581ea51773efbad7f0a17a6bc3292
brucepang <brucepang0618@gmail.com>
2024-05-22 08:21:10 +0800

    feature(common#logger): update Logger, add SimpleLogger.java

--- prpc-common/src/main/java/com/brucepang/prpc/logger/Logger.java
+++ prpc-common/src/main/java/com/brucepang/prpc/logger/Logger.java
@@ -12,11 +12,39 @@ public interface Logger {
     void debug(String message);
 
     /**
+     * Logs an error with debug log level.
+     * @param e log this cause
+     */
+    void debug(Throwable e);
+
+    /**
+     * Logs an error with debug log level.
+     * @param message log this message
+     * @param e log this cause
+     */
+    void debug(String message, Throwable e);
+
+
+
+    /**
      * Logs a message with info log level.
      *
      * @param message log this message
      */
     void info(String message);
+
+    /**
+     * Logs an error with info log level.
+     * @param e log this cause
+     */
+    void info(Throwable e);
+
+    /**
+     * Logs an error with info log level.
+     * @param message log this message
+     * @param e log this cause
+     */
+    void info(String message, Throwable e);
 
     /**
      * Logs a message with warn log level.
@@ -26,11 +54,37 @@ public interface Logger {
     void warn(String message);
 
     /**
+     * Logs an error with warn log level.
+     * @param e log this cause
+     */
+    void warn(Throwable e);
+
+    /**
+     * Logs an error with warn log level.
+     * @param message log this message
+     * @param e log this cause
+     */
+    void warn(String message, Throwable e);
+
+    /**
      * Logs a message with error log level.
      *
      * @param message log this message
      */
     void error(String message);
+
+    /**
+     * Logs an error with error log level.
+     * @param e log this cause
+     */
+    void error(Throwable e);
+
+    /**
+     * Logs an error with error log level.
+     * @param message log this message
+     * @param e log this cause
+     */
+    void error(String message, Throwable e);
 
 
 }
