c309561ca03c65cdc758e69028d932e6ea410c59
brucepang <brucepang0618@gmail.com>
2024-06-17 23:06:24 +0800

    feature(Holder): add Holder

---
+++ prpc-common/src/main/java/com/brucepang/prpc/util/Holder.java
@@ -0,0 +1,18 @@
+package com.brucepang.prpc.util;
+
+/**
+ * Helper Class for hold a value.
+ */
+public class Holder<T> {
+
+    private volatile T value;
+
+    public void set(T value) {
+        this.value = value;
+    }
+
+    public T get() {
+        return value;
+    }
+
+}
