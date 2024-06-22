760b78209899f3494a68bc374ccdfa156774c292
brucepang <brucepang0618@gmail.com>
2024-06-22 20:26:33 +0800

    [feature]: add DisableInject.java

--- prpc-common/src/main/java/com/brucepang/prpc/extension/inject/DisableInject.java
+++ prpc-common/src/main/java/com/brucepang/prpc/extension/inject/DisableInject.java
@@ -2,6 +2,9 @@ package com.brucepang.prpc.extension.inject;
 
 import java.lang.annotation.*;
 
+/**
+ * Disable Inject
+ */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
