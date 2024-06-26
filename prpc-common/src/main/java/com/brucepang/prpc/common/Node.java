497365392920819c88652ff8c3a93d08b4bf74c5
brucepang <brucepang0618@gmail.com>
2024-06-27 06:48:05 +0800

    [refactor]: Node Init

--- prpc-common/src/main/java/com/brucepang/prpc/common/Node.java
+++ prpc-common/src/main/java/com/brucepang/prpc/common/Node.java
@@ -1,7 +1,28 @@
 package com.brucepang.prpc.common;
 
+import com.brucepang.prpc.URL;
+
 /**
  * @author BrucePang
  */
 public interface Node {
+
+    /**
+     * get url.
+     *
+     * @return url.
+     */
+    URL getUrl();
+
+    /**
+     * is available.
+     *
+     * @return available.
+     */
+    boolean isAvailable();
+
+    /**
+     * destroy.
+     */
+    void destroy();
 }
