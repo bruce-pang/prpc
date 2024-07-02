53737a2bf37b621852593a42d5163137b1633717
brucepang <brucepang0618@gmail.com>
2024-07-02 08:07:02 +0800

    [feature]: InstantiationStrategy.instantiate

--- prpc-common/src/main/java/com/brucepang/prpc/beans/strategy/InstantiationStrategy.java
+++ prpc-common/src/main/java/com/brucepang/prpc/beans/strategy/InstantiationStrategy.java
@@ -2,6 +2,8 @@ package com.brucepang.prpc.beans.strategy;
 
 import com.brucepang.prpc.scope.model.ScopeModelAccessor;
 
+import java.lang.reflect.Constructor;
+
 /**
  * @author BrucePang
  */
@@ -14,5 +16,11 @@ public class InstantiationStrategy {
 
     public InstantiationStrategy(ScopeModelAccessor scopeModelAccessor) {
         this.scopeModelAccessor = scopeModelAccessor;
+    }
+
+    public <T> T instantiate(Class<T> type) throws ReflectiveOperationException {
+        Constructor<?> targetConstructor = type.getConstructor();
+        // todo: add more logic here
+        return (T) targetConstructor.newInstance();
     }
 }
