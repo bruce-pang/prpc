112340a01a28a671f1beb1c4efc56fb201daa5fe
brucepang <brucepang0618@gmail.com>
2024-07-05 01:04:34 +0800

    [feature]: ExtensionLoader.createAdaptiveExtensionClassCode

--- prpc-common/src/main/java/com/brucepang/prpc/extension/ExtensionLoader.java
+++ prpc-common/src/main/java/com/brucepang/prpc/extension/ExtensionLoader.java
@@ -480,11 +480,46 @@ public class ExtensionLoader<T> {
         return cachedAdaptiveClass = createAdaptiveExtensionClass();
     }
 
+
     private Class<?> createAdaptiveExtensionClass() {
         // Adaptive Classes' ClassLoader should be the same with Real SPI interface classes' ClassLoader
         ClassLoader classLoader = type.getClassLoader();
-        // todo: strategy to create adaptive extension class
+        // Generate the adaptive class
+        String code = createAdaptiveExtensionClassCode();
+        // todo compile the code
         return null;
+
+    }
+
+    private String createAdaptiveExtensionClassCode() {
+        StringBuilder code = new StringBuilder();
+        code.append("package ").append(type.getPackage().getName()).append(";");
+        code.append("import ").append(ExtensionLoader.class.getName()).append(";");
+        code.append("public class ").append(type.getSimpleName()).append("$Adaptive implements ").append(type.getCanonicalName()).append(" {");
+        code.append("public ").append(type.getSimpleName()).append("$Adaptive(){}");
+        Method[] methods = type.getMethods();
+        for (Method method : methods) {
+            code.append("public ").append(method.getReturnType().getCanonicalName()).append(" ").append(method.getName()).append("(");
+            Class<?>[] parameterTypes = method.getParameterTypes();
+            for (int i = 0; i < parameterTypes.length; i++) {
+                if (i > 0) {
+                    code.append(",");
+                }
+                code.append(parameterTypes[i].getCanonicalName()).append(" arg").append(i);
+            }
+            code.append("){");
+            code.append("String methodName = \"").append(method.getName()).append("\";");
+            code.append("String[] argTypes = new String[").append(parameterTypes.length).append("];");
+            for (int i = 0; i < parameterTypes.length; i++) {
+                code.append("argTypes[").append(i).append("] = arg").append(i).append(".getClass().getName();");
+            }
+            code.append("ExtensionLoader loader = ExtensionLoader.getExtensionLoader(").append(type.getCanonicalName()).append(".class);");
+            code.append("ExtensionAccessor accessor = loader.getExtensionAccessor();");
+            code.append("return (").append(method.getReturnType().getCanonicalName()).append(")accessor.invoke(methodName, argTypes, new Object[]{").append(Stream.of(parameterTypes).map(p -> "arg" + p).collect(Collectors.joining(","))).append("});");
+            code.append("}");
+        }
+        code.append("}");
+        return code.toString();
     }
 
 
