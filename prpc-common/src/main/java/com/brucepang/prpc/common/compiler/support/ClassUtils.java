afd7481f1cae00a086b46b4238973839a2b6e273
brucepang <brucepang0618@gmail.com>
2024-07-06 23:43:46 +0800

    [feature]: add Compiler and ClassUtils

---
+++ prpc-common/src/main/java/com/brucepang/prpc/common/compiler/support/ClassUtils.java
@@ -0,0 +1,42 @@
+package com.brucepang.prpc.common.compiler.support;
+
+/**
+ * ClassUtils. (Tool, Static, ThreadSafe)
+ * @author BrucePang
+ */
+public class ClassUtils {
+    public static final String CLASS_EXTENSION = ".class";
+
+    public static final String JAVA_EXTENSION = ".java";
+
+    private static final int JIT_LIMIT = 5 * 1024;
+
+    private ClassUtils() {
+    }
+
+    public static Class<?> loadClass(String className, ClassLoader classLoader) throws ClassNotFoundException {
+        return Class.forName(className, true, classLoader);
+    }
+
+    public static <T> T newInstance(String className, ClassLoader classLoader) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        Class<?> clazz = loadClass(className, classLoader);
+        return (T) clazz.newInstance();
+    }
+
+    public static String getClassName(Class<?> clazz) {
+        return clazz.getSimpleName();
+    }
+
+    public static String getPackageName(Class<?> clazz) {
+        return clazz.getPackage().getName();
+    }
+
+    public static boolean isAssignable(Class<?> parent, Class<?> child) {
+        return parent.isAssignableFrom(child);
+    }
+
+    public static String getResourcePath(Class<?> clazz, String resourceName) {
+        return clazz.getResource(resourceName).getPath();
+    }
+
+}
