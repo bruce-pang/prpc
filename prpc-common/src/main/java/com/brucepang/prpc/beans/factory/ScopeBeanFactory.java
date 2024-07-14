f771d0ebcf9539b9a176438d06c58a84a8052380
brucepang <brucepang0618@gmail.com>
2024-07-15 00:03:32 +0800

    [improve]: getBean()

--- prpc-common/src/main/java/com/brucepang/prpc/beans/factory/ScopeBeanFactory.java
+++ prpc-common/src/main/java/com/brucepang/prpc/beans/factory/ScopeBeanFactory.java
@@ -4,10 +4,14 @@ import com.brucepang.prpc.extension.ExtensionAccessor;
 import com.brucepang.prpc.extension.ExtensionPostProcessor;
 import com.brucepang.prpc.logger.Logger;
 import com.brucepang.prpc.logger.LoggerFactory;
+import com.brucepang.prpc.util.StrUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
 
 /**
  * A bean factory
@@ -26,18 +30,48 @@ public class ScopeBeanFactory {
 
     private final List<ExtensionPostProcessor> extensionPostProcessors;
 
+    private final List<BeanInfo> registeredBeanInfos = new CopyOnWriteArrayList<>();
+
     public ScopeBeanFactory(ScopeBeanFactory parent, ExtensionAccessor extensionAccessor) {
         this.parent = parent;
         this.extensionAccessor = extensionAccessor;
         extensionPostProcessors = extensionAccessor.getExtensionMgt().getExtensionPostProcessors();
     }
 
-    public <T> T getBean(String name) {
-        BeanDefinition<T> beanDefinition = (BeanDefinition<T>) beanDefinitionMap.get(name);
-        if (beanDefinition == null) {
-            throw new IllegalArgumentException("No bean named " + name + " is defined");
+    public <T> T getBean(String name, Class<T> type) {
+        T bean = getBeanInternal(name, type);
+        if (bean == null && parent != null) {
+            return parent.getBean(name, type);
         }
-        return beanDefinition.getBeanInstance();
+        return bean;
+    }
+
+    public <T> T getBeanInternal(String name, Class<T> type) {
+        if (type == Object.class) {
+            return null;
+        }
+        Optional<BeanInfo> matchedBean = registeredBeanInfos.stream()
+                .filter(beanInfo -> type.isAssignableFrom(beanInfo.instance.getClass()))
+                .filter(beanInfo -> StrUtil.isEquals(beanInfo.name, name))
+                .findFirst();
+
+        if (matchedBean.isPresent()) {
+            return (T) matchedBean.get().instance;
+        }
+
+        // Handling the case where the bean name does not match but there is only one candidate
+        List<BeanInfo> candidates = registeredBeanInfos.stream()
+                .filter(beanInfo -> type.isAssignableFrom(beanInfo.instance.getClass()))
+                .collect(Collectors.toList());
+
+        if (candidates.size() == 1) {
+            return (T) candidates.get(0).instance;
+        } else if (candidates.size() > 1) {
+            List<String> candidateBeanNames = candidates.stream().map(beanInfo -> beanInfo.name).collect(Collectors.toList());
+            throw new RuntimeException("expected single matching bean but found " + candidates.size() + " candidates for type [" + type.getName() + "]: " + candidateBeanNames);
+        }
+
+        return null;
     }
 
     public <T> void registerBeanDefinition(String name, BeanDefinition<T> beanDefinition) {
@@ -50,6 +84,16 @@ public class ScopeBeanFactory {
 
     public void destroyBeans() {
         beanDefinitionMap.values().forEach(BeanDefinition::destroy);
+    }
+
+    static class BeanInfo {
+        private final String name;
+        private final Object instance;
+
+        public BeanInfo(String name, Object instance) {
+            this.name = name;
+            this.instance = instance;
+        }
     }
 
 }
