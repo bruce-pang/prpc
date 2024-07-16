cff55c1eac2c2694485572b6b83758801294a4a4
brucepang <brucepang0618@gmail.com>
2024-07-16 23:05:35 +0800

    [improve]:  implement this class to process the extension before or after initialization

--- prpc-common/src/main/java/com/brucepang/prpc/scope/processor/ScopeModelAwareExtensionProcessor.java
+++ prpc-common/src/main/java/com/brucepang/prpc/scope/processor/ScopeModelAwareExtensionProcessor.java
@@ -1,12 +1,37 @@
 package com.brucepang.prpc.scope.processor;
 
 import com.brucepang.prpc.extension.ExtensionPostProcessor;
+import com.brucepang.prpc.extension.inject.ScopeModelAware;
+import com.brucepang.prpc.scope.model.*;
 
 /**
- * todo: implement this class to process the extension before or after initialization
+ *
  * @author BrucePang
  */
-public class ScopeModelAwareExtensionProcessor implements ExtensionPostProcessor{
+public class ScopeModelAwareExtensionProcessor implements ExtensionPostProcessor, ScopeModelAccessor {
+    private ScopeModel scopeModel;
+    private GlobalModel globalModel;
+    private ApplicationModel applicationModel;
+    private ModuleModel moduleModel;
+
+    public ScopeModelAwareExtensionProcessor(ScopeModel scopeModel) {
+        this.scopeModel = scopeModel;
+        initialize();
+    }
+
+    private void initialize() {
+        if (scopeModel instanceof GlobalModel) {
+            globalModel = (GlobalModel) scopeModel;
+        } else if (scopeModel instanceof ApplicationModel) {
+            applicationModel = (ApplicationModel) scopeModel;
+            globalModel = applicationModel.getGlobalModel();
+        } else if (scopeModel instanceof ModuleModel) {
+            moduleModel = (ModuleModel) scopeModel;
+            applicationModel = moduleModel.getApplicationModel();
+            globalModel = applicationModel.getGlobalModel();
+        }
+    }
+
     @Override
     public Object postProcessBeforeInitialization(Object instance, String name) throws Exception {
         return ExtensionPostProcessor.super.postProcessBeforeInitialization(instance, name);
@@ -14,6 +39,39 @@ public class ScopeModelAwareExtensionProcessor implements ExtensionPostProcessor
 
     @Override
     public Object postProcessAfterInitialization(Object instance, String name) throws Exception {
-        return ExtensionPostProcessor.super.postProcessAfterInitialization(instance, name);
+        if (instance instanceof ScopeModelAware){
+            ScopeModelAware modelAware = (ScopeModelAware) instance;
+            modelAware.setScopeModel(scopeModel);
+            if (this.moduleModel != null) {
+                modelAware.setModuleModel(this.moduleModel);
+            }
+            if (this.applicationModel != null) {
+                modelAware.setApplicationModel(this.applicationModel);
+            }
+            if (this.globalModel != null) {
+                modelAware.setGlobalModel(this.globalModel);
+            }
+        }
+        return instance;
+    }
+
+    @Override
+    public ScopeModel getScopeModel() {
+        return scopeModel;
+    }
+
+    @Override
+    public GlobalModel getGlobalModel() {
+        return globalModel;
+    }
+
+    @Override
+    public ApplicationModel getApplicationModel() {
+        return applicationModel;
+    }
+
+    @Override
+    public ModuleModel getModuleModel() {
+        return moduleModel;
     }
 }
