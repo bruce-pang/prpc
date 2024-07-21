7474fbffc076cc3923130d6a216159e1c881f069
brucepang <brucepang0618@gmail.com>
2024-07-21 23:42:23 +0800

    [feature]:  add Prioritized

---
+++ prpc-common/src/main/java/com/brucepang/prpc/lang/Prioritized.java
@@ -0,0 +1,62 @@
+package com.brucepang.prpc.lang;
+
+import java.util.Comparator;
+import static java.lang.Integer.compare;
+
+/**
+ * @description: An interface for comparing priorities for sort
+ * @author BrucePang
+ */
+public interface Prioritized extends Comparable<Prioritized>{
+    /**
+     * The maximum priority
+     */
+    int MAX_PRIORITY = Integer.MIN_VALUE;
+
+    /**
+     * The minimum priority
+     */
+    int MIN_PRIORITY = Integer.MAX_VALUE;
+
+    /**
+     * Normal Priority
+     */
+    int NORMAL_PRIORITY = 0;
+
+    /**
+     * 
+     */
+    Comparator<Object> COMPARATOR = (arg1, arg2) -> {
+        boolean b1 = arg1 instanceof Prioritized;
+        boolean b2 = arg2 instanceof Prioritized;
+        if (b1 && !b2) {        // arg1 is Prioritized, arg2 is not
+            return -1;
+        } else if (b2 && !b1) { // arg2 is Prioritized, arg1 is not
+            return 1;
+        } else if (b1 && b2) {  //  arg1 and arg2 both are Prioritized
+            return ((Prioritized) arg1).compareTo((Prioritized) arg2);
+        } else {                // no different
+            return 0;
+        }
+    };
+
+    /**
+     * Get the priority
+     * @return the default is {@link #NORMAL_PRIORITY}
+     */
+    default int getPriority() {
+        return NORMAL_PRIORITY;
+    }
+
+    /**
+     * Compare the priority of two objects for sorting them:
+     *  if return value is less than 0, then the first object has a higher priority
+     *  if return value is greater than 0, then the second object has a higher priority
+     * @param that the object to compare with
+     * @return the result of the comparison
+     */
+    @Override
+    default int compareTo(Prioritized that) {
+        return compare(this.getPriority(), that.getPriority());
+    }
+}
