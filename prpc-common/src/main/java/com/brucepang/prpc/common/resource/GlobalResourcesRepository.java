f9067b385299f7660190c6be667418e055eac7f2
brucepang <brucepang0618@gmail.com>
2024-07-28 21:43:33 +0800

    [feature]: getExecutorService()

--- prpc-common/src/main/java/com/brucepang/prpc/common/resource/GlobalResourcesRepository.java
+++ prpc-common/src/main/java/com/brucepang/prpc/common/resource/GlobalResourcesRepository.java
@@ -2,6 +2,10 @@ package com.brucepang.prpc.common.resource;
 
 import com.brucepang.prpc.logger.Logger;
 import com.brucepang.prpc.logger.LoggerFactory;
+import com.brucepang.prpc.util.NamedThreadFactory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Global resource repository between all framework models.
@@ -11,7 +15,7 @@ public class GlobalResourcesRepository {
     private static final Logger logger = LoggerFactory.getLogger(GlobalResourcesRepository.class);
 
     private volatile static GlobalResourcesRepository instance;
-
+    private volatile ExecutorService executorService;
     private GlobalResourcesRepository() {
     }
 
@@ -25,4 +29,19 @@ public class GlobalResourcesRepository {
         }
         return instance;
     }
+
+    public ExecutorService getExecutorService() {
+        if (executorService == null || executorService.isShutdown()) {
+            synchronized (this) {
+                if (executorService == null || executorService.isShutdown()) {
+                    if (logger.isInfoEnabled()) {
+                        logger.info("Creating global shared handler ...");
+                    }
+                    executorService = Executors.newCachedThreadPool(new NamedThreadFactory("Dubbo-global-shared-handler", true));
+                }
+            }
+        }
+        return executorService;
+    }
+
 }
