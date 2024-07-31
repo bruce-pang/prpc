7962dcd6a0f5cd4be20acb791621c26804ff2aaa
brucepang <brucepang0618@gmail.com>
2024-07-31 22:33:20 +0800

    [feature]: setRef()

--- prpc-common/src/main/java/com/brucepang/prpc/util/ClassLoaderResourceLoader.java
+++ prpc-common/src/main/java/com/brucepang/prpc/util/ClassLoaderResourceLoader.java
@@ -1,9 +1,23 @@
 package com.brucepang.prpc.util;
 
+import com.brucepang.prpc.common.resource.GlobalResourcesRepository;
+import com.brucepang.prpc.logger.Logger;
+import com.brucepang.prpc.logger.LoggerFactory;
+
+import java.io.IOException;
+import java.lang.ref.SoftReference;
+import java.lang.reflect.Field;
+import java.net.URL;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+
 /**
  * @author BrucePang
  */
 public class ClassLoaderResourceLoader {
+    private static final Logger logger = LoggerFactory.getLogger(ClassLoaderResourceLoader.class);
+    private static SoftReference<Map<ClassLoader, Map<String, Set<URL>>>> classLoaderResourcesCache = null;
 
     public static ClassLoader getClassLoader() {
         return ClassLoaderResourceLoader.class.getClassLoader();
@@ -96,4 +110,14 @@ public class ClassLoaderResourceLoader {
             }
         }
     }
+
+    private static void setRef(URL url) {
+        try {
+            Field field = URL.class.getDeclaredField("ref");
+            field.setAccessible(true);
+            field.set(url, UUID.randomUUID().toString());
+        } catch (Throwable ignore) {
+        }
+    }
+
 }
