15648551e8a4116cb01a3ff7d1763b524f7ae1b0
brucepang <brucepang0618@gmail.com>
2024-08-05 00:01:01 +0800

    [improve]: forName(), classForName(), arrayForName()

--- prpc-common/src/main/java/com/brucepang/prpc/common/compiler/support/ClassUtils.java
+++ prpc-common/src/main/java/com/brucepang/prpc/common/compiler/support/ClassUtils.java
@@ -48,4 +48,77 @@ public class ClassUtils {
             throw new IllegalArgumentException(e.getMessage(), e);
         }
     }
+
+    public static Class<?> forName(String[] packages, String className) {
+        try {
+            return classForName(className);
+        } catch (ClassNotFoundException e) {
+            if (packages != null && packages.length > 0) {
+                for (String pkg : packages) {
+                    try {
+                        return classForName(pkg + "." + className);
+                    } catch (ClassNotFoundException ignore) {
+                    }
+                }
+            }
+            throw new IllegalStateException(e.getMessage(), e);
+        }
+    }
+
+    public static Class<?> classForName(String className) throws ClassNotFoundException {
+        switch (className) {
+            case "boolean":
+                return boolean.class;
+            case "byte":
+                return byte.class;
+            case "char":
+                return char.class;
+            case "short":
+                return short.class;
+            case "int":
+                return int.class;
+            case "long":
+                return long.class;
+            case "float":
+                return float.class;
+            case "double":
+                return double.class;
+            case "boolean[]":
+                return boolean[].class;
+            case "byte[]":
+                return byte[].class;
+            case "char[]":
+                return char[].class;
+            case "short[]":
+                return short[].class;
+            case "int[]":
+                return int[].class;
+            case "long[]":
+                return long[].class;
+            case "float[]":
+                return float[].class;
+            case "double[]":
+                return double[].class;
+            default:
+        }
+        try {
+            return arrayForName(className);
+        } catch (ClassNotFoundException e) {
+            // try to load from java.lang package
+            if (className.indexOf('.') == -1) {
+                try {
+                    return arrayForName("java.lang." + className);
+                } catch (ClassNotFoundException ignore) {
+                    // ignore, let the original exception be thrown
+                }
+            }
+            throw e;
+        }
+    }
+
+    private static Class<?> arrayForName(String className) throws ClassNotFoundException {
+        return Class.forName(className.endsWith("[]")
+                ? "[L" + className.substring(0, className.length() - 2) + ";"
+                : className, true, Thread.currentThread().getContextClassLoader());
+    }
 }
