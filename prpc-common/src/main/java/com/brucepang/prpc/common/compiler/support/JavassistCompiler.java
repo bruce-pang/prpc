da447503bcc2a10b3d652b29894f981b0ccdbcd3
brucepang <brucepang0618@gmail.com>
2024-08-09 22:59:53 +0800

    [feature]: JavassistCompiler.java

--- prpc-common/src/main/java/com/brucepang/prpc/common/compiler/support/JavassistCompiler.java
+++ prpc-common/src/main/java/com/brucepang/prpc/common/compiler/support/JavassistCompiler.java
@@ -1,5 +1,13 @@
 package com.brucepang.prpc.common.compiler.support;
 
+import com.brucepang.prpc.common.bytecode.PrpcLoaderClassPath;
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.LoaderClassPath;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -16,4 +24,62 @@ public class JavassistCompiler extends AbstractCompiler {
     private static final Pattern METHODS_PATTERN = Pattern.compile("\n(private|public|protected)\\s+");
 
     private static final Pattern FIELD_PATTERN = Pattern.compile("[^\n]+=[^\n]+;");
+
+    @Override
+    public Class<?> doCompile(Class<?> neighbor, ClassLoader classLoader, String name, String source) throws Throwable {
+        CtClassBuilder builder = new CtClassBuilder();
+        builder.setClassName(name);
+
+        // process imported classes
+        Matcher matcher = IMPORT_PATTERN.matcher(source);
+        while (matcher.find()) {
+            builder.addImports(matcher.group(1).trim());
+        }
+
+        // process extended super class
+        matcher = EXTENDS_PATTERN.matcher(source);
+        if (matcher.find()) {
+            builder.setSuperClassName(matcher.group(1).trim());
+        }
+
+        // process implemented interfaces
+        matcher = IMPLEMENTS_PATTERN.matcher(source);
+        if (matcher.find()) {
+            String[] ifaces = matcher.group(1).trim().split("\\,");
+            Arrays.stream(ifaces).forEach(i -> builder.addInterface(i.trim()));
+        }
+
+        // process constructors, fields, methods
+        String body = source.substring(source.indexOf('{') + 1, source.length() - 1);
+        String[] methods = METHODS_PATTERN.split(body);
+        String className = ClassUtils.getSimpleClassName(name);
+        Arrays.stream(methods).map(String::trim).filter(m -> !m.isEmpty()).forEach(method -> {
+            if (method.startsWith(className)) {
+                builder.addConstructor("public " + method);
+            } else if (FIELD_PATTERN.matcher(method).matches()) {
+                builder.addField("private " + method);
+            } else {
+                builder.addMethod("public " + method);
+            }
+        });
+
+        // compile
+        CtClass cls = builder.build(classLoader);
+
+        ClassPool cp = cls.getClassPool();
+        if (classLoader == null) {
+            classLoader = cp.getClassLoader();
+        }
+        cp.insertClassPath(new LoaderClassPath(classLoader));
+        cp.insertClassPath(new PrpcLoaderClassPath());
+
+        try {
+            return cp.toClass(cls, neighbor, classLoader, JavassistCompiler.class.getProtectionDomain());
+        } catch (Throwable t) {
+            if (!(t instanceof CannotCompileException)) {
+                return cp.toClass(cls, classLoader, JavassistCompiler.class.getProtectionDomain());
+            }
+            throw t;
+        }
+    }
 }
