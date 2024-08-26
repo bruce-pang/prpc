baa2aaade34b8b034f2a3a09fbfbfd23d7cda4af
brucepang <brucepang0618@gmail.com>
2024-08-26 22:18:37 +0800

    [feature]: add Endpoint

---
+++ prpc-rpc/prpc-rpc-api/src/main/java/com/brucepang/prpc/rpc/Endpoint.java
@@ -0,0 +1,14 @@
+package com.brucepang.prpc.rpc;
+
+import com.brucepang.prpc.common.URL;
+
+/**
+ * @author BrucePang
+ */
+public interface Endpoint {
+    /**
+     * get url
+     * @return
+     */
+    URL getUrl();
+}
