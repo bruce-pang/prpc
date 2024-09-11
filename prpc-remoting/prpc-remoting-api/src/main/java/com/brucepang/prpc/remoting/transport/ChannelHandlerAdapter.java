521a30581507f9497293290481ee559153ec4c43
brucepang <brucepang0618@gmail.com>
2024-09-11 12:59:31 +0800

    [feature]: add sent() received

--- prpc-remoting/prpc-remoting-api/src/main/java/com/brucepang/prpc/remoting/transport/ChannelHandlerAdapter.java
+++ prpc-remoting/prpc-remoting-api/src/main/java/com/brucepang/prpc/remoting/transport/ChannelHandlerAdapter.java
@@ -2,6 +2,7 @@ package com.brucepang.prpc.remoting.transport;
 
 import com.brucepang.prpc.remoting.Channel;
 import com.brucepang.prpc.remoting.ChannelHandler;
+import com.brucepang.prpc.remoting.RemotingException;
 
 /**
  * @author BrucePang
@@ -14,6 +15,16 @@ public class ChannelHandlerAdapter implements ChannelHandler {
 
     @Override
     public void disconnected(Channel channel) {
+
+    }
+
+    @Override
+    public void sent(Channel channel, Object message) throws RemotingException {
+
+    }
+
+    @Override
+    public void received(Channel channel, Object message) throws RemotingException {
 
     }
 }
