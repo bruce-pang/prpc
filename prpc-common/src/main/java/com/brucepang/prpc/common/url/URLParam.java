63a1229dda914d25e209b72922027d73b51cdc4a
brucepang <brucepang0618@gmail.com>
2024-09-16 23:14:31 +0800

    [improve]: URLParam

--- prpc-common/src/main/java/com/brucepang/prpc/common/url/URLParam.java
+++ prpc-common/src/main/java/com/brucepang/prpc/common/url/URLParam.java
@@ -1,6 +1,7 @@
 package com.brucepang.prpc.common.url;
 
 import java.util.BitSet;
+import java.util.Map;
 
 /**
  * @author BrucePang
@@ -21,4 +22,36 @@ public class URLParam {
         KEY = null;
         VALUE = null;
     }
+
+    public static URLParam parse(Map<String, String> params) {
+        return parse(params, null);
+    }
+
+    private static URLParam parse(Map<String, String> params, String dynamicParam) {
+        URLParam urlParam = new URLParam();
+        if (params == null) {
+            return urlParam;
+        }
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            if (key != null && value != null) {
+                urlParam.put(key, value);
+            }
+        }
+        return urlParam;
+
+    }
+
+    private void put(String key, String value) {
+        int offset = 0;
+        if (value == null) {
+            offset = -1;
+        } else {
+            offset = 0;
+        }
+        KEY.set(offset);
+        VALUE[offset] = offset;
+    }
+
 }
