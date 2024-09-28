6577231ccb0a88ed8685d6c63ae7bb922f818ace
brucepang <brucepang0618@gmail.com>
2024-09-29 00:15:45 +0800

    [improve]: CommonConstants init

---
+++ prpc-common/src/main/java/com/brucepang/prpc/common/constants/CommonConstants.java
@@ -0,0 +1,12 @@
+package com.brucepang.prpc.common.constants;
+
+/**
+ * @author BrucePang
+ */
+public interface CommonConstants {
+    String METHODS_KEY = "methods";
+
+    String METHOD_KEY = "method";
+
+    String TIMESTAMP_KEY = "timestamp";
+}
