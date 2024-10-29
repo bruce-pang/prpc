27c43aaf83e1d14f90112e35c3eee91f40354cef
brucepang <brucepang0618@gmail.com>
2024-10-29 13:48:12 +0800

    [init]: ConfigurationUtils

---
+++ prpc-common/src/main/java/com/brucepang/prpc/config/ConfigurationUtils.java
@@ -0,0 +1,14 @@
+package com.brucepang.prpc.config;
+
+/**
+ * @author BrucePang
+ */
+public class ConfigurationUtils {
+
+    /**
+     * Forbids instantiation.
+     */
+    private ConfigurationUtils() {
+        throw new UnsupportedOperationException("No instance of 'ConfigurationUtils' for you! ");
+    }
+}
