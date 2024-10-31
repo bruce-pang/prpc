15909a3b12cf74ed57afcbe5c9c18b54af0938d6
brucepang <brucepang0618@gmail.com>
2024-11-01 07:54:38 +0800

    [improve]: CommonConstants

--- prpc-common/src/main/java/com/brucepang/prpc/common/constants/CommonConstants.java
+++ prpc-common/src/main/java/com/brucepang/prpc/common/constants/CommonConstants.java
@@ -15,4 +15,11 @@ public interface CommonConstants {
     String DEFAULT_KEY_PREFIX = "default.";
 
     Pattern COMMA_SPLIT_PATTERN = Pattern.compile("\\s*[,]+\\s*");
+
+    Pattern SEMICOLON_SPLIT_PATTERN = Pattern.compile("\\s*[;]+\\s*");
+
+    Pattern EQUAL_SPLIT_PATTERN = Pattern.compile("\\s*[=]+\\s*");
+
+    String PRPC_LABELS = "prpc.labels";
+    String PRPC_ENV_KEYS = "prpc.env.keys";
 }
