31ed6e527ce1291835b5b022c0cd7c6c6750593a
brucepang <brucepang0618@gmail.com>
2024-10-31 22:40:50 +0800

    [init]: ConfigurationUtils.getScopeModelOrDefaultApplicationModel

--- prpc-common/src/main/java/com/brucepang/prpc/config/ConfigurationUtils.java
+++ prpc-common/src/main/java/com/brucepang/prpc/config/ConfigurationUtils.java
@@ -1,5 +1,8 @@
 package com.brucepang.prpc.config;
 
+import com.brucepang.prpc.scope.model.ApplicationModel;
+import com.brucepang.prpc.scope.model.ScopeModel;
+
 /**
  * ConfigurationUtils
  * @author BrucePang
@@ -12,4 +15,12 @@ public final class ConfigurationUtils {
     private ConfigurationUtils() {
         throw new UnsupportedOperationException("No instance of 'ConfigurationUtils' for you! ");
     }
+
+    private static ScopeModel getScopeModelOrDefaultApplicationModel(ScopeModel realScopeModel) {
+        if (realScopeModel == null) {
+            return ApplicationModel.defaultModel();
+        }
+        return realScopeModel;
+    }
+
 }
