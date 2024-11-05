645c6e1e76a43258ba7bcdd715dcb774b064225b
brucepang <brucepang0618@gmail.com>
2024-11-05 23:26:30 +0800

    [init]: Environment.java

---
+++ prpc-common/src/main/java/com/brucepang/prpc/config/Environment.java
@@ -0,0 +1,27 @@
+package com.brucepang.prpc.config;
+
+import com.brucepang.prpc.common.context.ApplicationExt;
+import com.brucepang.prpc.common.context.LifecycleAdapter;
+
+/**
+ * @author BrucePang
+ */
+public class Environment extends LifecycleAdapter implements ApplicationExt {
+
+    public static final String NAME = "environment";
+
+    @Override
+    public void initialize() throws IllegalStateException {
+
+    }
+
+    @Override
+    public void start() throws IllegalStateException {
+
+    }
+
+    @Override
+    public void destroy() throws IllegalStateException {
+
+    }
+}
