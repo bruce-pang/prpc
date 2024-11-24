1993ff0fecf311c647f59b94d0fbaa8881295dfb
brucepang <brucepang0618@gmail.com>
2024-11-24 11:11:19 +0800

    [feature]: PRPCReference.java

---
+++ prpc-common/src/main/java/com/brucepang/prpc/config/annotation/PRPCReference.java
@@ -0,0 +1,13 @@
+package com.brucepang.prpc.config.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * An annotation used for referencing a`PRPC` service.
+ * @author BrucePang
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+public @interface PRPCReference {
+}
