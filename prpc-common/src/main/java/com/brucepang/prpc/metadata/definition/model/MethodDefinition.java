2e5e94fb06fc2dfb3b8fcd9762936b7b8861901e
brucepang <brucepang0618@gmail.com>
2024-12-07 08:39:07 +0800

    [init]: MethodDefinition.java

---
+++ prpc-common/src/main/java/com/brucepang/prpc/metadata/definition/model/MethodDefinition.java
@@ -0,0 +1,10 @@
+package com.brucepang.prpc.metadata.definition.model;
+
+/**
+ * @author BrucePang
+ */
+public class MethodDefinition {
+    private String name;
+    private String[] parameterTypes;
+    private String returnType;
+}
