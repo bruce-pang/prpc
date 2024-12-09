9b61338d292669ac8127f1058cbd37b18ae51682
brucepang <brucepang0618@gmail.com>
2024-12-09 22:18:12 +0800

    [improve]: FullServiceDefinition.java

--- prpc-common/src/main/java/com/brucepang/prpc/metadata/definition/model/FullServiceDefinition.java
+++ prpc-common/src/main/java/com/brucepang/prpc/metadata/definition/model/FullServiceDefinition.java
@@ -1,8 +1,26 @@
 package com.brucepang.prpc.metadata.definition.model;
 
+import java.util.Map;
+
 /**
  * FullServiceDefinition
  * @author BrucePang
  */
-public class FullServiceDefinition {
+public class FullServiceDefinition extends ServiceDefinition {
+
+    private Map<String, String> parameters;
+
+    public Map<String, String> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(Map<String, String> parameters) {
+        this.parameters = parameters;
+    }
+
+    @Override
+    public String toString() {
+        return "FullServiceDefinition{" + "parameters=" + parameters + "} " + super.toString();
+    }
+
 }
