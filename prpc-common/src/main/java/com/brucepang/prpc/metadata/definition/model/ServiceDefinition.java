8371011a839175bc8034a6d7c29a2102426a8f08
brucepang <brucepang0618@gmail.com>
2024-12-18 08:07:38 +0800

    [improve]: ServiceDefinition.getter() and setter()

--- prpc-common/src/main/java/com/brucepang/prpc/metadata/definition/model/ServiceDefinition.java
+++ prpc-common/src/main/java/com/brucepang/prpc/metadata/definition/model/ServiceDefinition.java
@@ -3,6 +3,8 @@ package com.brucepang.prpc.metadata.definition.model;
 import com.brucepang.prpc.common.compiler.support.ClassUtils;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -30,4 +32,55 @@ public class ServiceDefinition implements Serializable {
      * the definitions of type
      */
     private List<TypeDefinition> types;
+
+    /**
+     * the definitions of annotations
+     */
+    private List<String> annotations;
+
+    public List<String> getAnnotations() {
+        if (annotations == null) {
+            annotations = Collections.emptyList();
+        }
+        return annotations;
+    }
+
+    public void setAnnotations(List<String> annotations) {
+        this.annotations = annotations;
+    }
+
+    public String getCanonicalName() {
+        return canonicalName;
+    }
+
+    public void setCanonicalName(String canonicalName) {
+        this.canonicalName = canonicalName;
+    }
+
+    public String getCodeSource() {
+        return codeSource;
+    }
+
+    public void setCodeSource(String codeSource) {
+        this.codeSource = codeSource;
+    }
+
+    public List<MethodDefinition> getMethods() {
+        return methods;
+    }
+
+    public void setMethods(List<MethodDefinition> methods) {
+        this.methods = methods;
+    }
+
+    public List<TypeDefinition> getTypes() {
+        if (types == null) {
+            types = new ArrayList<>();
+        }
+        return types;
+    }
+
+    public void setTypes(List<TypeDefinition> types) {
+        this.types = types;
+    }
 }
