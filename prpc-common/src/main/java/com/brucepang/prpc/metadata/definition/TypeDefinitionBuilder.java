c3b857c9b38f6a19c5d91d8b8fba31f016cc5660
brucepang <brucepang0618@gmail.com>
2024-12-22 00:08:32 +0800

    [improve]: TypeDefinitionBuilder.java

--- prpc-common/src/main/java/com/brucepang/prpc/metadata/definition/TypeDefinitionBuilder.java
+++ prpc-common/src/main/java/com/brucepang/prpc/metadata/definition/TypeDefinitionBuilder.java
@@ -1,8 +1,15 @@
 package com.brucepang.prpc.metadata.definition;
 
+import com.brucepang.prpc.logger.Logger;
+import com.brucepang.prpc.logger.LoggerFactory;
+import com.brucepang.prpc.metadata.definition.builder.TypeBuilder;
+
+import java.util.List;
+
 /**
  * @author BrucePang
  */
 public class TypeDefinitionBuilder {
-
+    private static final Logger logger = LoggerFactory.getLogger(TypeDefinitionBuilder.class);
+    public static List<TypeBuilder> BUILDERS;
 }
