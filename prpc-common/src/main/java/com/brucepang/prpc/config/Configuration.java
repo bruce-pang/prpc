480bf2079c7e12dfd98c822dfd7d63c33c9f7ebc
brucepang <brucepang0618@gmail.com>
2024-12-26 07:01:25 +0800

    [improve]: add Logger

--- prpc-common/src/main/java/com/brucepang/prpc/config/Configuration.java
+++ prpc-common/src/main/java/com/brucepang/prpc/config/Configuration.java
@@ -1,9 +1,14 @@
 package com.brucepang.prpc.config;
 
+import com.brucepang.prpc.logger.Logger;
+import com.brucepang.prpc.logger.LoggerFactory;
+
 /**
  * Configuration interface, to fetch the value for the specified key.
  * @author BrucePang
  */
 public interface Configuration {
+    Logger logger = LoggerFactory.getLogger(Configuration.class);
+
     Object getInternalProperty(String key);
 }
