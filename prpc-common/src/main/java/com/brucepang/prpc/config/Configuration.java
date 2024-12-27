5b190ff5a012116e7ec2421c1913d6132ee579de
brucepang <brucepang0618@gmail.com>
2024-12-27 21:03:40 +0800

    [improve]: add function getProperty()

--- prpc-common/src/main/java/com/brucepang/prpc/config/Configuration.java
+++ prpc-common/src/main/java/com/brucepang/prpc/config/Configuration.java
@@ -11,4 +11,18 @@ public interface Configuration {
     Logger logger = LoggerFactory.getLogger(Configuration.class);
 
     Object getInternalProperty(String key);
+
+    /**
+     * Gets a property from the configuration. The default value will return if the configuration doesn't contain
+     * the mapping for the specified key.
+     *
+     * @param key property to retrieve
+     * @param defaultValue default value
+     * @return the value to which this configuration maps the specified key, or default value if the configuration
+     * contains no mapping for this key.
+     */
+    default Object getProperty(String key, Object defaultValue) {
+        Object value = getInternalProperty(key);
+        return value != null ? value : defaultValue;
+    }
 }
