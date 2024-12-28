46d642bbe08854f4a6534b5fe79acff66f1dab01
brucepang <brucepang0618@gmail.com>
2024-12-28 10:00:34 +0800

    [improve]: getProperty()

--- prpc-common/src/main/java/com/brucepang/prpc/config/Configuration.java
+++ prpc-common/src/main/java/com/brucepang/prpc/config/Configuration.java
@@ -25,4 +25,24 @@ public interface Configuration {
         Object value = getInternalProperty(key);
         return value != null ? value : defaultValue;
     }
+
+    /**
+     * Gets a property from the configuration. This is the most basic get
+     * method for retrieving values of properties. In a typical implementation
+     * of the {@code Configuration} interface the other get methods (that
+     * return specific data types) will internally make use of this method. On
+     * this level variable substitution is not yet performed. The returned
+     * object is an internal representation of the property value for the passed
+     * in key. It is owned by the {@code Configuration} object. So a caller
+     * should not modify this object. It cannot be guaranteed that this object
+     * will stay constant over time (i.e. further update operations on the
+     * configuration may change its internal state).
+     *
+     * @param key property to retrieve
+     * @return the value to which this configuration maps the specified key, or
+     * null if the configuration contains no mapping for this key.
+     */
+    default Object getProperty(String key) {
+        return getProperty(key, null);
+    }
 }
