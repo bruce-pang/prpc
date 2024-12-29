53cb2deeabc24a9e89949a47193375414d508177
brucepang <brucepang0618@gmail.com>
2024-12-29 23:48:19 +0800

    [improve]: convert()

--- prpc-common/src/main/java/com/brucepang/prpc/config/Configuration.java
+++ prpc-common/src/main/java/com/brucepang/prpc/config/Configuration.java
@@ -45,4 +45,40 @@ public interface Configuration {
     default Object getProperty(String key) {
         return getProperty(key, null);
     }
+
+    default <T> T convert(Class<T> cls, String key, T defaultValue) {
+        // we only process String properties for now
+        String value = (String) getProperty(key);
+
+        if (value == null) {
+            return defaultValue;
+        }
+
+        Object obj = value;
+        if (cls.isInstance(value)) {
+            return cls.cast(value);
+        }
+
+        if (Boolean.class.equals(cls) || Boolean.TYPE.equals(cls)) {
+            obj = Boolean.valueOf(value);
+        } else if (Number.class.isAssignableFrom(cls) || cls.isPrimitive()) {
+            if (Integer.class.equals(cls) || Integer.TYPE.equals(cls)) {
+                obj = Integer.valueOf(value);
+            } else if (Long.class.equals(cls) || Long.TYPE.equals(cls)) {
+                obj = Long.valueOf(value);
+            } else if (Byte.class.equals(cls) || Byte.TYPE.equals(cls)) {
+                obj = Byte.valueOf(value);
+            } else if (Short.class.equals(cls) || Short.TYPE.equals(cls)) {
+                obj = Short.valueOf(value);
+            } else if (Float.class.equals(cls) || Float.TYPE.equals(cls)) {
+                obj = Float.valueOf(value);
+            } else if (Double.class.equals(cls) || Double.TYPE.equals(cls)) {
+                obj = Double.valueOf(value);
+            }
+        } else if (cls.isEnum()) {
+            obj = Enum.valueOf(cls.asSubclass(Enum.class), value);
+        }
+
+        return cls.cast(obj);
+    }
 }
