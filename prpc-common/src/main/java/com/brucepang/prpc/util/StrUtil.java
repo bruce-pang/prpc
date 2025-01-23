c4b508a410a39f346da2522f2de819122f97fb8b
brucepang <brucepang0618@gmail.com>
2025-01-24 00:07:45 +0800

    [improve]: StrUtil.camelToSplitName()

--- prpc-common/src/main/java/com/brucepang/prpc/util/StrUtil.java
+++ prpc-common/src/main/java/com/brucepang/prpc/util/StrUtil.java
@@ -226,6 +226,40 @@ public final class StrUtil {
         return str.chars().allMatch(ch -> ch == split || isWord((char) ch));
     }
 
+    public static String camelToSplitName(String camelName, String split) {
+        if (isEmpty(camelName)) {
+            return camelName;
+        }
+        if (!isWord(camelName)) {
+            // convert Ab-Cd-Ef to ab-cd-ef
+            if (isSplitCase(camelName, split.charAt(0))) {
+                return camelName.toLowerCase();
+            }
+            // not camel case
+            return camelName;
+        }
+
+        StringBuilder buf = null;
+        for (int i = 0; i < camelName.length(); i++) {
+            char ch = camelName.charAt(i);
+            if (ch >= 'A' && ch <= 'Z') {
+                if (buf == null) {
+                    buf = new StringBuilder();
+                    if (i > 0) {
+                        buf.append(camelName, 0, i);
+                    }
+                }
+                if (i > 0) {
+                    buf.append(split);
+                }
+                buf.append(Character.toLowerCase(ch));
+            } else if (buf != null) {
+                buf.append(ch);
+            }
+        }
+        return buf == null ? camelName.toLowerCase() : buf.toString().toLowerCase();
+    }
+
 
 
 }
