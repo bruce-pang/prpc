5fa526bd6624d9d158347db5510dbbaa9c63d4f1
brucepang <brucepang0618@gmail.com>
2025-01-26 16:47:17 +0800

    [improve]: ConfigurationUtils..getSubProperties()

--- prpc-common/src/main/java/com/brucepang/prpc/config/ConfigurationUtils.java
+++ prpc-common/src/main/java/com/brucepang/prpc/config/ConfigurationUtils.java
@@ -2,7 +2,13 @@ package com.brucepang.prpc.config;
 
 import com.brucepang.prpc.scope.model.ApplicationModel;
 import com.brucepang.prpc.scope.model.ScopeModel;
+import com.brucepang.prpc.util.CollectionUtils;
 import com.brucepang.prpc.util.StrUtil;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * ConfigurationUtils
@@ -41,6 +47,42 @@ public final class ConfigurationUtils {
 
     public static boolean isEmptyValue(Object value) {
         return value == null || value instanceof String && StrUtil.isBlank((String) value);
+    }
+
+    private static <V extends Object> Map<String, V> getSubProperties(
+            Map<String, V> configMap, String prefix, Map<String, V> resultMap) {
+        if (!prefix.endsWith(".")) {
+            prefix += ".";
+        }
+
+        if (null == resultMap) {
+            resultMap = new LinkedHashMap<>();
+        }
+
+        if (CollectionUtils.isNotEmptyMap(configMap)) {
+            Map<String, V> copy;
+            synchronized (configMap) {
+                copy = new HashMap<>(configMap);
+            }
+            for (Map.Entry<String, V> entry : copy.entrySet()) {
+                String key = entry.getKey();
+                V val = entry.getValue();
+                if (StrUtil.startsWithIgnoreCase(key, prefix)
+                        && key.length() > prefix.length()
+                        && !ConfigurationUtils.isEmptyValue(val)) {
+
+                    String k = key.substring(prefix.length());
+                    // convert camelCase/snake_case to kebab-case
+                    String newK = StrUtil.convertToSplitName(k, "-");
+                    resultMap.putIfAbsent(newK, val);
+                    if (!Objects.equals(k, newK)) {
+                        resultMap.putIfAbsent(k, val);
+                    }
+                }
+            }
+        }
+
+        return resultMap;
     }
 
 }
