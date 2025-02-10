bdb46d1ed314241bd163cf0b349ea17b0aba8a9d
brucepang <brucepang0618@gmail.com>
2025-02-10 22:37:28 +0800

    [feature]: TypeDefinition.build()

--- prpc-common/src/main/java/com/brucepang/prpc/metadata/definition/TypeDefinitionBuilder.java
+++ prpc-common/src/main/java/com/brucepang/prpc/metadata/definition/TypeDefinitionBuilder.java
@@ -1,13 +1,15 @@
 package com.brucepang.prpc.metadata.definition;
 
+import com.brucepang.prpc.common.compiler.support.ClassUtils;
 import com.brucepang.prpc.logger.Logger;
 import com.brucepang.prpc.logger.LoggerFactory;
+import com.brucepang.prpc.metadata.definition.builder.DefaultTypeBuilder;
 import com.brucepang.prpc.metadata.definition.builder.TypeBuilder;
+import com.brucepang.prpc.metadata.definition.model.TypeDefinition;
 import com.brucepang.prpc.scope.model.GlobalModel;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.lang.reflect.Type;
+import java.util.*;
 
 /**
  * @author BrucePang
@@ -15,6 +17,7 @@ import java.util.Set;
 public class TypeDefinitionBuilder {
     private static final Logger logger = LoggerFactory.getLogger(TypeDefinitionBuilder.class);
     public static List<TypeBuilder> BUILDERS;
+    private final Map<String, TypeDefinition> typeCache = new HashMap<>();
 
     public static void initBuilders(GlobalModel model) {
         Set<TypeBuilder> tbs = model.getExtensionLoader(TypeBuilder.class).getSupportedExtensionInstances();
@@ -39,4 +42,22 @@ public class TypeDefinitionBuilder {
         return build(type, clazz, typeCache);
     }
 
+    public static TypeDefinition build(Type type, Class<?> clazz, Map<String, TypeDefinition> typeCache) {
+        TypeBuilder builder = getGenericTypeBuilder(clazz);
+        TypeDefinition td;
+
+        if (clazz.isPrimitive() || ClassUtils.isSimpleType(clazz)) { // changed since 2.7.6
+            td = new TypeDefinition(clazz.getCanonicalName());
+            typeCache.put(clazz.getCanonicalName(), td);
+        } else if (builder != null) {
+            td = builder.build(type, clazz, typeCache);
+        } else {
+            td = DefaultTypeBuilder.build(clazz, typeCache);
+        }
+        return td;
+    }
+
+    public List<TypeDefinition> getTypeDefinitions() {
+        return new ArrayList<>(typeCache.values());
+    }
 }
